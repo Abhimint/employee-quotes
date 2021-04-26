35ec20dd94174706151c5fde66d28e7218307098
Abhi <Abhishek.Mukherjee@ibm.com>
2021-04-26 02:40:01 -0400

    [ Cleanup ] - Cleaned up routes file

--- routes/employees.js
+++ routes/employees.js
@@ -3,14 +3,8 @@
 const express = require('express');
 var router = express.Router();
 var employee = require('../controller/EmployeeController');
-// const create = require('../controller/EmployeeController');
-// const bodyParser = require('body-parser');
-
 // TODO: Look up if router is only get or post? 
 // Are these Rest calls or just associating to rest calls?
-
-// router.use(bodyParser.json());
-// router.post('/create', create.create);
 
 // Fetch all employee
 router.get('/', employee.list);

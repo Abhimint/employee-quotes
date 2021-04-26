9830e1a9dc2da5c681e8bdb5a14a8a4db6923db6
Abhi <Abhishek.Mukherjee@ibm.com>
2021-04-26 12:15:16 -0400

    [ Major Refactor for Index.js + Baseline Error Handler/View ] - index.js: Reverted back to using atlas db for ease of learning -- Updated Db connection config using this article --> "https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/mongoose" -- Updated execution of .use() vs. .require() (imports) vs .set() as proper execution was not implemented before and kept breaking the connection (model require to be called before .use and .set) | Clarified on a SO post - views/error.ejs: Added view to render error onto UI

--- index.js
+++ index.js
@@ -1,39 +1,94 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const app = express();
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+// const app = express();
 
-const Employee = require('./models/Employee');
-var employees = require('./routes/employees');
+// const Employee = require('./models/Employee');
+// var employees = require('./routes/employees');
 
-//Server
-app.listen(3000,() => {
-    console.log('Server is Up');
-});
+// //Server
+// app.listen(3000,() => {
+//     console.log('Server is Up');
+// });
 
-// any USES to be added before the CRUD ops
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/employees', employees);
-
-// GET
-app.get('/', (req,res) => {
-    res.send('hello world');
-});
-
-// TODO: Figure out how db configuration is handled (permissioning...) without exposing creds
+// // TODO: Figure out how db configuration is handled (permissioning...) without exposing creds
 // const connectionString = 'mongodb+srv://employeeAdmin:TJ0lI4OvfOuekT9t@cluster0.lvbvh.mongodb.net/employee-quotes?retryWrites=true&w=majority';
 
-const connectionString = 'mongodb://localhost/Employee';
-// MongoDB connection
-mongoose.connect(connectionString, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-}).then(client => {
-    console.log('Connected to database');
-    const db = client.db('employee-quotes');
-    const quotesCollection = db.collection('quotes');
+// // const connectionString = 'mongodb://127.0.0.1/employee';
+// // MongoDB connection
+// mongoose.connect(connectionString, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true
+// }).then(() => {
+//     console.log('Connected to database');
+// }).catch(err => console.error('Database connection could not be established', err));
 
-    // Express request handlers
+// var db = mongoose.connection;
 
+// // view engine setup
+// app.set('view engine', 'ejs');
 
-}).catch(err => console.error('Database connection could not be established', err));
+// // any USES to be added before the CRUD ops
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use('/employees', employees);
+
+// // GET
+// app.get('/', (req,res) => {
+//     res.send('hello world');
+// });
+
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+const Employee = require('./models/Employee');
+mongoose.Promise = global.Promise;
+
+const connectionString = 'mongodb+srv://employeeAdmin:SCVK1fOuPrfDUyzf@cluster0.lvbvh.mongodb.net/employee-quotes?retryWrites=true&w=majority';
+
+mongoose.connect(connectionString, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() =>  {
+    console.log('connection succesful');  
+    // Initiate connection on port 3000
+    app.listen(3000, () => {
+        console.log('listening on port 3000');    
+    });
+    }).catch((err) => console.error(err));
+
+var index = require('./routes/index');
+var employees = require('./routes/employees');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/employees', employees);
+
+// TODO: Build error handler
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// TODO: Build environment files
+// error handler
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;

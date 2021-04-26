9e37a5de7ff39b9139db362becea5084a9ed3e47
Abhi <Abhishek.Mukherjee@ibm.com>
2021-04-26 15:41:44 -0400

    [ Trying out Node-Fetch Module ] - index.js: POC for the fetch. API consumes double .then() - package.json: Added the fetch module

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 const Employee = require('./models/Employee');
 mongoose.Promise = global.Promise;
+// const fetch = require('node-fetch');
+
+const https = require('https');
+
+const QUOTE_URL = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
+
+var app = express();
+app.locals.moment = require('moment');
+
+// TODO: Look into a way to add some lifecycle hooks/observables to give order to execution
 
 const connectionString = 'mongodb+srv://employeeAdmin:SCVK1fOuPrfDUyzf@cluster0.lvbvh.mongodb.net/employee-quotes?retryWrites=true&w=majority';
 
@@ -12,17 +22,31 @@ mongoose.connect(connectionString, { useUnifiedTopology: true, useNewUrlParser: 
     console.log('connection succesful');  
     // Initiate connection on port 3000
     app.listen(3000, () => {
-        console.log('listening on port 3000');    
+        console.log('listening on port 3000');
+        // Fetch Ron Swanson Quote
+        // fetch(QUOTE_URL).then( res => res.json() ).then(res => console.log(res));
+        // fetch(QUOTE_URL).resolve(res => {
+        //   console.log('Ron Swanson Quote', res.json());
+        // });
+        // https.get(QUOTE_URL, res => {
+        //   res.setEncoding('utf8');
+        //   let quote = "";
+        //   res.on('data', data => {
+        //     quote += data;
+        //   });
+        //   res.on('end', () => {
+        //     quote = JSON.parse(quote);
+        //     console.log('Ron Swanson Quote', quote);
+        //   })
+        // });
     });
     }).catch((err) => console.error(err));
 
 var index = require('./routes/index');
 var employees = require('./routes/employees');
 
-var app = express();
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.resolve(path.join(__dirname, 'views')));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
